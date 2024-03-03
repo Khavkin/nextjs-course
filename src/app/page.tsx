5d4dad1e13ebe7c3e0b89904b1f1de991ac892d8
Khavkin <hdm@ukr.net>
2024-03-03 14:44:47 +0200

    added dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,32 @@
-import styles from "./page.module.css";
-import course from "./course.json";
-import { Lesson } from "./lesson";
+import dynamic from 'next/dynamic';
+const ClientComponent = dynamic(
+  () => import('./some-component'), {ssr:false}
+)
 
-async function getAllCourses(): Promise<typeof course> {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(course), 1000);
-  })
-}
+// import styles from "./page.module.css";
+// import course from "./course.json";
+// import { Lesson } from "./lesson";
+
+// async function getAllCourses(): Promise<typeof course> {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(course), 1000);
+//   })
+// }
 
 export default async function Home() {
-  const courses = await getAllCourses();
+  // const courses = await getAllCourses();
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Test</h1>
-      <ul>
+      <ClientComponent/>
+      {/* <ul>
         {course.lessons.map((lesson) => {
           return <li key={lesson.name}>
             <Lesson title = {lesson.name} shortSummary={lesson.shortSummary}/>
             
           </li>
         })}
-      </ul>
+      </ul> */}
       
     </main>
   );
---
+++ src/app/some-component.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export default function SomeComponent() {
+    console.log('some component')
+    return <p>Some super component</p>
+}
